7d2b26b65e82f90159e308b7941bc8e98b2d8432
Akshay2602-Ak <takshaykumar26022004@gmail.com>
2026-05-19 12:42:47 +0530

    code pushed

---
+++ app.py
@@ -0,0 +1,18 @@
+def classify_text(text):
+    positive_words = ["good", "great", "happy", "excellent"]
+    negative_words = ["bad", "sad", "poor", "angry"]
+
+    text = text.lower()
+
+    if any(word in text for word in positive_words):
+        return "Positive"
+    elif any(word in text for word in negative_words):
+        return "Negative"
+    else:
+        return "Neutral"
+
+
+user_text = input("Enter your text: ")
+result = classify_text(user_text)
+print("AI Prediction:", result)
+
