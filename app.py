0f1be9d83be165dee738c0cde1d4bce7f8fef241
Akshay2602-Ak <takshaykumar26022004@gmail.com>
2026-05-19 12:48:01 +0530

    Code improved

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 def classify_text(text):
-    positive_words = ["good", "great", "happy", "excellent"]
-    negative_words = ["bad", "sad", "poor", "angry"]
-
+    positive_words = ["good", "great", "happy", "excellent", "awesome", "love"]
+    negative_words = ["bad", "sad", "poor", "angry", "hate", "worst"]
     text = text.lower()
 
     if any(word in text for word in positive_words):
