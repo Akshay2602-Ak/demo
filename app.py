ed3e48118298098c9a27a673cff1d66ed6f810e8
Akshay2602-Ak <takshaykumar26022004@gmail.com>
2026-05-22 13:01:24 +0530

    old version

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 def classify_text(text):
-    positive_words = ["good", "great", "happy", "excellent", "awesome"]
-    negative_words = ["bad", "sad", "poor", "angry", "worst"]
-
+    positive_words = ["good", "great", "happy", "excellent","Nala iruku","super"]
+    negative_words = ["bad", "sad", "poor", "angry", "hate", "worst","waste","nalla illa","ussu(vikash)marri iruku"]
     text = text.lower()
 
     if any(word in text for word in positive_words):
@@ -12,4 +11,8 @@ def classify_text(text):
         return "Neutral"
 
 
-print(classify_text("This is awesome"))
+user_text = input("Enter your text: ")
+result = classify_text(user_text)
+print("AI Prediction:", result)
+
+ 
